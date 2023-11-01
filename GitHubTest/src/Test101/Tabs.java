43068486b53a1484d83206a6c650a8729669aba9
Hanc038 <147174781+Hanc038@users.noreply.github.com>
2023-11-01 09:45:30 -0500

    Add files via upload
    Testing THIS

---
+++ GitHubTest/src/Test101/Tabs.java
@@ -0,0 +1,11 @@
+package Test101;
+public class Tabs {
+    public static void main(String[] args) {
+        
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+        
+        
+    }
+}
---
+++ GitHubTest/src/githubtest/Piss.java
@@ -0,0 +1,6 @@
+package githubtest;
+public class Piss {
+    public static void main(String[] args) {
+        System.out.println("PISS");
+    }
+}
